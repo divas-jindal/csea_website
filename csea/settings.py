b0e2a63b741b75d5382e8dbc0095f21cd9ad1efa
DIVAS JINDAL <divasjindal@gmail.com>
2020-09-08 23:58:08 +0530

    Update settings.py

--- csea/settings.py
+++ csea/settings.py
@@ -19,7 +19,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'divasjindal@gmail.com'
-EMAIL_HOST_PASSWORD = 'timetotravel'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
