e081ee1c3b2ad1b457dceb6a04f9d7b336cc7f0b
Touqeer Pathan <touqeer.pathan289@gmail.com>
2020-11-21 14:47:44 +0530

    Fixed feedback form + email notification to sender and admin

--- cseaweb/forms.py
+++ cseaweb/forms.py
@@ -1,8 +1,7 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
-from django.contrib.auth.models import User
-class contactForms(forms.Form):
-    name = forms.CharField(label='Name:',required = True ,max_length=100,help_text = 'max 100 chars allowed')
-    email = forms.EmailField(required = True,label='E-mail:')
-    comment = forms.CharField(label='Feedback:',required = True,widget=forms.Textarea)
-    
+from .models import feedback
+class contactForms(forms.ModelForm):
+    class Meta:
+        model = feedback
+        fields = '__all__'
+        # exclude = ()
