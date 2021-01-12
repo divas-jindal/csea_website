552cec2b5183594b80b4390ee6358f364966dedc
Mohd Sufiyan Ansari <addsufiyan@gmail.com>
2021-01-12 23:15:35 +0530

    Resolved CPL UI Issues

--- cseaweb/cpl_teams/teams_data.py
+++ cseaweb/cpl_teams/teams_data.py
@@ -4,35 +4,35 @@ TEAMS = {
             
                 {
                     "Name": "Karthik Kothuri",
-                    "Position": "Team Leader",
+                   # "Position": "Team Leader",
                     "Course and Year": "B.Tech 4yr",
                     "CodeChef Username": "kkothuri",
                     "CodeForces Username": "kkothuri"
                 },
                 {
                     "Name": "Anand R Raut",
-                    "Position": "Coder",
+                    # "Position": "Coder",
                     "Course and Year": "B.Tech 3yr",
                     "CodeChef Username": "anand_2137",
                     "CodeForces Username": "codes2137"
                 },
                 {
                     "Name": "Mohit Talreja",
-                    "Position": "Coder",
+                    # "Position": "Coder",
                     "Course and Year": "B.Tech 4yr",
                     "CodeChef Username": "mohit_talreja",
                     "CodeForces Username": "talrejamohit99"
                 },
                 {
                     "Name": "Apurv Jain",
-                    "Position": "Coder",
+                    # "Position": "Coder",
                     "Course and Year": "B.Tech 2yr",
                     "CodeChef Username": "ap821901",
                     "CodeForces Username": "Apurv_1781"
                 },
                 {
                     "Name": "Yash Kumawat",
-                    "Position": "Coder",
+                    # "Position": "Coder",
                     "Course and Year": "B.Tech 2yr",
                     "CodeChef Username": "yash3240",
                     "CodeForces Username": "kyash"
@@ -41,35 +41,35 @@ TEAMS = {
         "010": [
             {
                 "Name": "Raj Sahu",
-                "Position": "Team Leader",
+                # "Position": "Team Leader",
                 "Course and Year": "B.Tech 3yr",
                 "CodeChef Username": "raj_sahu123",
                 "CodeForces Username": "lowercase_guy"
             },
             {
                 "Name": "Snigdh Kamal",
-                "Position": "Coder",
+                # "Position": "Coder",
                 "Course and Year": "B.Tech 3yr",
                 "CodeChef Username": "snigdh_op",
                 "CodeForces Username": "snigdh_op"
             },
             {
                 "Name": "Ritik Agrawal",
-                "Position": "Coder",
+                # "Position": "Coder",
                 "Course and Year": "B.Tech 3yr",
                 "CodeChef Username": "starter821824",
                 "CodeForces Username": "ritikagrawal2k18"
             },
             {
                 "Name": "Sushith Reddy",
-                "Position": "Coder",
+                # "Position": "Coder",
                 "Course and Year": "B.Tech 2yr",
                 "CodeChef Username": "sushithreddy75",
                 "CodeForces Username": "N/A"
             },
             {
                 "Name": "Nikhil Pal",
-                "Position": "Coder",
+                # "Position": "Coder",
                 "Course and Year": "B.Tech 2yr",
                 "CodeChef Username": "nikhilpal2468",
                 "CodeForces Username": "Nikhil__Pal"
@@ -78,35 +78,35 @@ TEAMS = {
         "011": [
             {
                 "Name": "Gaurav Singh",
-                "Position": "Team Leader",
+                # "Position": "Team Leader",
                 "Course and Year": "B.Tech 3yr",
                 "CodeChef Username": "ahambrahmasmi",
                 "CodeForces Username": "gaurav_512"
             },
             {
                 "Name": "Yejarla Surya Varahala Raju",
-                "Position": "Coder",
+                # "Position": "Coder",
                 "Course and Year": "B.Tech 4yr",
                 "CodeChef Username": "N/A",
                 "CodeForces Username": "N/A"
             },
             {
                 "Name": "Sriphani Vardhan",
-                "Position": "Coder",
+                # "Position": "Coder",
                 "Course and Year": "B.Tech 3yr",
                 "CodeChef Username": "skatecode007",
                 "CodeForces Username": "skatecode007"
             },
             {
                 "Name": "ShreeRam Mohanty",
-                "Position": "Coder",
+                # "Position": "Coder",
                 "Course and Year": "B.Tech 2yr",
                 "CodeChef Username": "ram0553",
                 "CodeForces Username": "Ram0553"
             },
             {
                 "Name": "Anshuman Mishra",
-                "Position": "Coder",
+                # "Position": "Coder",
                 "Course and Year": "B.Tech 2yr",
                 "CodeChef Username": "boruto_uz",
                 "CodeForces Username": "shivanshuman02"
@@ -115,35 +115,35 @@ TEAMS = {
         "100": [
             {
                 "Name": "Ankit Baid",
-                "Position": "Team Leader",
+                # "Position": "Team Leader",
                 "Course and Year": "B.Tech 3yr",
                 "CodeChef Username": "ankitbaid7",
                 "CodeForces Username": "ankitbaid7"
             },
             {
                 "Name": "Palash Gupta",
-                "Position": "Coder",
+                # "Position": "Coder",
                 "Course and Year": "B.Tech 3yr",
                 "CodeChef Username": "palash_07_",
                 "CodeForces Username": "palash_07"
             },
             {
                 "Name": "Mohit Kadyan",
-                "Position": "Coder",
+                # "Position": "Coder",
                 "Course and Year": "B.Tech 3yr",
                 "CodeChef Username": "neophyte_01",
                 "CodeForces Username": "neo_phyte"
             },
             {
                 "Name": "Saswat Das",
-                "Position": "Coder",
+                # "Position": "Coder",
                 "Course and Year": "B.Tech 2yr",
                 "CodeChef Username": "daschiku37",
                 "CodeForces Username": "daschiku37"
             },
             {
                 "Name": "PANKAJ GOYAL",
-                "Position": "Coder",
+                # "Position": "Coder",
                 "Course and Year": "B.Tech 2yr",
                 "CodeChef Username": "pankaj_2407",
                 "CodeForces Username": "pankaj_2407"
@@ -152,35 +152,35 @@ TEAMS = {
         "101": [
             {
                 "Name": "Atul Raghuwanshi",
-                "Position": "Team Leader",
+                # "Position": "Team Leader",
                 "Course and Year": "B.Tech 3yr",
                 "CodeChef Username": "NA",
                 "CodeForces Username": "Atul_Raghuwanshi"
             },
             {
                 "Name": "Surya Ramanand Sharma",
-                "Position": "Coder",
+                # "Position": "Coder",
                 "Course and Year": "B.Tech 3yr",
                 "CodeChef Username": "Surya9900",
                 "CodeForces Username": "Lamb of Motorhead"
             },
             {
                 "Name": "Himanshu Manwani",
-                "Position": "Coder",
+                # "Position": "Coder",
                 "Course and Year": "B.Tech 3yr",
                 "CodeChef Username": "himans1999",
                 "CodeForces Username": "himans1999"
             },
             {
                 "Name": "Harshit Bansal",
-                "Position": "Coder",
+                # "Position": "Coder",
                 "Course and Year": "B.Tech 2yr",
                 "CodeChef Username": "harshit135",
                 "CodeForces Username": "harshit135"
             },
             {
                 "Name": "Pokuri Gnana Samhitha",
-                "Position": "Coder",
+                # "Position": "Coder",
                 "Course and Year": "B.Tech 2yr",
                 "CodeChef Username": "santhu_1",
                 "CodeForces Username": "GnanaSamhitha"
@@ -189,35 +189,35 @@ TEAMS = {
         "110": [
                 {
                     "Name": "Aviral Gupta",
-                    "Position": "Team Leader",
+                    # "Position": "Team Leader",
                     "Course and Year": "B.Tech 4yr",
                     "CodeChef Username": "hoacrux",
                     "CodeForces Username": "hoacrux"
                 },
                 {
                     "Name": "Tushar Sain",
-                    "Position": "Coder",
+                    # "Position": "Coder",
                     "Course and Year": "B.Tech 3yr",
                     "CodeChef Username": "sain2801",
                     "CodeForces Username": "sain2801"
                 },
                 {
                     "Name": "Ram Prabodh Induri",
-                    "Position": "Coder",
+                    # "Position": "Coder",
                     "Course and Year": "B.Tech 4yr",
                     "CodeChef Username": "raminduri",
                     "CodeForces Username": "RamPrabodhInduri"
                 },
                 {
                     "Name": "Mohd Sufiyan Ansari",
-                    "Position": "Coder",
+                    # "Position": "Coder",
                     "Course and Year": "B.Tech 2yr",
                     "CodeChef Username": "sufiyan133",
                     "CodeForces Username": "imsuffi"
                 },
                 {
                     "Name": "Induri Saikalyan",
-                    "Position": "Coder",
+                    # "Position": "Coder",
                     "Course and Year": "B.Tech 2yr",
                     "CodeChef Username": "red_rex",
                     "CodeForces Username": "t_dog"
